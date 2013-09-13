a2d6c3067074cef7e14c21b6e03954eaedfee775
learnICT <kibrisli7@gmail.com>
2013-09-13 21:21:03 +0100

    new webcam file

---
+++ camera/webcamSnap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-------------------------------------------------------------------------------
+# Name:        webcamSnap
+# Purpose:
+#
+# Author:      Ali Mulla
+#
+# Created:     13/09/2013
+# Copyright:   (c) Ali 2013
+# Licence:     Free to use, please give credit
+#-------------------------------------------------------------------------------
+
+# This line imports the ability to use commands that can be used in the
+# terminal window
+import os
+
+def takeSnap(filename="snap.jpg"):
+    # This part is the command that would have been written into terminal
+    os.system("fswebcam --no-banner -r 600x480 -d /dev/video0 %s" % filename
+
+#takes the photo
+takeSnap()
