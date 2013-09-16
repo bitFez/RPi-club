535ddb29f21d49a64691302b627809ae2af16cb0
learnICT <kibrisli7@gmail.com>
2013-09-16 16:29:59 +0100

    Update webcamSnap.py
    Updated the commenting and changed the version to 1.0 since it has been functioning as intended from version 0.4

--- camera/webcamSnap.py
+++ camera/webcamSnap.py
@@ -6,14 +6,15 @@
 # Author:      Ali Mulla of www.learnICT.it
 #
 # Created:     13/09/2013
-# Version:     0.4
+# Version:     1.0
 # Licence:     Free to use, please give credit
 #-------------------------------------------------------------------------------
 
-# This line imports the ability to use commands that can be used in the
-# terminal window
+# This line imports the ability to use commands are used in the terminal window
 import os
 
+
+# This part defines a function for taking the snap with date stamp on filename
 def takeSnap(filename="snap_%d%m%y_%h%s.jpg"):
     # This part is the command that would have been written into terminal
     os.system ("fswebcam --no-banner -r 600x480 -d /dev/video0 %s" % filename)
