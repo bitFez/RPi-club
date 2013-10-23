d5bb5950a9ced3f7e01f490da3f635829d958be6
learnICT <kibrisli7@gmail.com>
2013-10-23 09:57:42 +0100

    Update PygameCam_test.py
    Defined timestamp better

--- camera/PygameCam_test.py
+++ camera/PygameCam_test.py
@@ -5,7 +5,7 @@
 # Purpose:     To take still images using a webcam and pygame
 #
 # Author:      Ali Mulla
-# Version:     0.3
+# Version:     0.4
 # Created:     22/10/2013
 # Copyright:   No Copyright
 # Thanks:      Thanks to Ajit Akar for the idea on CAS forums.
@@ -17,11 +17,14 @@ import datetime
 import pygame
 import pygame.camera
 from pygame.locals import *
+now = datetime.datetime.now()
 
 def takeSnap(filename= "webcam" + now.strftime("%Y-%m-%d %H:%M") + ".jpg"):
-pygame.init()
-pygame.camera.init()
-cam = pygame.camera.Camera("/dev/video0",(640,480))
-cam.start()
-image = cam.get_image()
-pygame.image.save(image, filename)
+    pygame.init()
+    pygame.camera.init()
+    cam = pygame.camera.Camera("/dev/video0",(640,480))
+    cam.start()
+    image = cam.get_image()
+    pygame.image.save(image, filename)
+
+takeSnap()
