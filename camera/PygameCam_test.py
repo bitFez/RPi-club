b66b8663f19dee07194ab277b521869d876bf56d
learnICT <kibrisli7@gmail.com>
2013-10-23 09:49:16 +0100

    Updated naming to add timestamp
    Dunno if it works yet

--- camera/PygameCam_test.py
+++ camera/PygameCam_test.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
+
+#-------------------------------------------------------------------------------
+# Name:        Pygame Webcam Snap
+# Purpose:     To take still images using a webcam and pygame
+#
+# Author:      Ali Mulla
+# Version:     0.3
+# Created:     22/10/2013
+# Copyright:   No Copyright
+# Thanks:      Thanks to Ajit Akar for the idea on CAS forums.
+#-------------------------------------------------------------------------------
+
 import sys
 import os
+import datetime
 import pygame
 import pygame.camera
 from pygame.locals import *
 
+def takeSnap(filename= "webcam" + now.strftime("%Y-%m-%d %H:%M") + ".jpg"):
 pygame.init()
 pygame.camera.init()
 cam = pygame.camera.Camera("/dev/video0",(640,480))
 cam.start()
 image = cam.get_image()
-pygame.image.save(image,'webcam.jpg')
+pygame.image.save(image, filename)
