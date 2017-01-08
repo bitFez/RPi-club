6790f3dc235325909e317dfa67888e6de807b768
learnICT <kibrisli7@gmail.com>
2017-01-08 01:05:45 +0000

    updated script

--- camera/webcamSnap.py
+++ camera/webcamSnap.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python
 #-------------------------------------------------------------------------------
 # Name:        webcamSnap
-# Purpose:
+# Purpose:     Takes a picture using a webcam on the raspberry pi from within Python
 #
 # Author:      Ali Mulla of www.learnICT.it
 #
 # Created:     13/09/2013
-# Version:     1.0
+# Version:     1.2
 # Licence:     Free to use, please give credit
 #-------------------------------------------------------------------------------
 
-# This line imports the ability to use commands are used in the terminal window
-import os
+# This line imports the ability to use commands that are used in the command line
+from subprocess import call
+# This line imports the time 
+from datetime import datetime
 
-
-# This part defines a function for taking the snap with date stamp on filename
-def takeSnap(filename="snap_%d%m%y_%h%s.jpg"):
-    # This part is the command that would have been written into terminal
-    os.system ("fswebcam --no-banner -r 600x480 -d /dev/video0 %s" % filename)
-
-#takes the photo
-takeSnap()
+# This part is the function which runs the webcam and captures and image
+def capture():
+    dt = datetime.now().isoformat() # This grabs current time
+    dtime = dt[0:4]+dt[5:7]+dt[8:10]+dt[11:13]+dt[14:16]+dt[17:19] # This formarts the time to add to filename
+    # this line runs the code that captures the image (adding the current time to filename)
+    call(["fswebcam", "-d","/dev/video0", "-r", "640x480", "--no-banner", "./%d.jpg" % int(dtime)])
+    
+# This line runs / calls the above function
+capture()
